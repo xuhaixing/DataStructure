8c9353e728a80b1bca3071bce5f193e327be5da1
xuhaixing <haixing1993@163.com>
2019-12-23 14:41:48 +0800

    leetcode

--- leetcode/leetcode-demo/src/main/java/com/xhx/lc/LC167.java
+++ leetcode/leetcode-demo/src/main/java/com/xhx/lc/LC167.java
@@ -5,24 +5,24 @@ public class LC167 {
     public static void main(String[] args) {
         LC167 lc167 = new LC167();
         int[] ints = lc167.twoSum(new int[]{-1, 1, 2, 3}, 3);
-        System.out.println(ints[0] + "   "+ ints[1]);
+        System.out.println(ints[0] + "   " + ints[1]);
     }
 
     public int[] twoSum(int[] numbers, int target) {
         int[] result = new int[2];
-        for(int i = 0, j = numbers.length-1; i < numbers.length; i++){
-            if(numbers[i]>target){
+        for (int i = 0, j = numbers.length - 1; i < numbers.length && i < j; i++) {
+            if (numbers[i] > target) {
                 return null;
             }
-            while (numbers[i]+numbers[j] > target){
+            while (numbers[i] + numbers[j] > target) {
                 j--;
-                if(j <= 0){
+                if (j <= 0) {
                     break;
                 }
             }
-            if(numbers[i]+numbers[j] == target){
-                result[0] = i+1;
-                result[1] = j+1;
+            if (numbers[i] + numbers[j] == target) {
+                result[0] = i + 1;
+                result[1] = j + 1;
                 break;
             }
         }
