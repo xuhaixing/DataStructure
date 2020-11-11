3023f1ce334b03b665f69dad968d9f604f365aba
xuhaixing <haixing1993@163.com>
2020-11-11 08:37:54 +0800

    lc25

---
+++ 10Array&LinkedList/leetcode/src/main/java/com/xhx/leetcode/LC25.java
@@ -0,0 +1,27 @@
+package com.xhx.leetcode;
+
+public class LC25 {
+
+    public ListNode reverseKGroup(ListNode head, int k) {
+
+        return null;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+}
